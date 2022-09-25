8ef6850a6997e7c5f9a6503bd1ba48b8c319624b
Balara00 <balarajayaweera@gmail.com>
2022-09-25 20:43:32 +0530

    demo student landing page

---
+++ local/workflow/classes/form/student_index.php
@@ -0,0 +1,43 @@
+<?php
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package     local_workflow
+ * @author
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+//moodleform is defined in formslib.php
+require_once("$CFG->libdir/formslib.php");
+
+class studentindex extends moodleform {
+    //Add elements to form
+    public function definition() {
+        global $CFG;
+
+        $mform = $this->_form; // Don't forget the underscore!
+
+        $t = $mform->addElement('button', 'createreqbtn', 'Create Request');
+        
+
+        // print_r($t);
+        // die();
+        // echo $data;
+
+    }
+    //Custom validation should be added here
+    function validation($data, $files) {
+        return array();
+    }
+}
