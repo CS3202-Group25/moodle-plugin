e12871d4b24dd00e35311f1586c865f43ada49bd
Balara00 <balarajayaweera@gmail.com>
2022-10-04 11:18:28 +0530

    changestate  to lowercase

--- local/workflow/update.php
+++ local/workflow/update.php
@@ -39,13 +39,13 @@ if($value === 'cancel') {
     $requestController->deleteRequest($requestId);
     redirect($CFG->wwwroot . '/mod/workflow/viewallrequests.php', "You have successfully deleted the request");
 }elseif ($value === 'approve'){
-    $requestController->changeStatus('Approved', $requestId, 'state');
+    $requestController->changeStatus('approved', $requestId, 'state');
     redirect($CFG->wwwroot . '/mod/workflow/viewallrequests.php', "You have approved the request");
 }elseif($value === 'disapprove'){
-    $requestController->changeStatus('Disapproved', $requestId, 'state');
+    $requestController->changeStatus('disapproved', $requestId, 'state');
     redirect($CFG->wwwroot . '/mod/workflow/viewallrequests.php', "You have approved the request");
 }elseif($value === 'forward'){
-    $requestController->changeStatus('Forwarded', $requestId, 'state');
+    $requestController->changeStatus('forwarded', $requestId, 'state');
 //    changeStatus('Lecturer', $requestId, 'receivedby');
     redirect($CFG->wwwroot . '/mod/workflow/viewallrequests.php', "You have forwarded the request to lecturer");
 }
